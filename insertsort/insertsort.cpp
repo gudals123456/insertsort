8402e70bd07c747a1bcc5a0e38e82f414022f61f
hyoungmin <gudals123456@gmail.com>
2015-01-15 17:32:18 +0900

    asdf

--- insertsort/insertsort.cpp
+++ insertsort/insertsort.cpp
@@ -1,6 +1,52 @@
 #include <stdio.h>
 
-void main () {
+void swap(int * a, int * b);
+void printarr(int * arr,int size);
 
-	printf("fucking git\n");
+void main() {
+
+	printf("hello C world!\n");
+	printf("Insert sort\n\n");
+
+	int arr[] = {34,27,35,1,23,19,85,67,4,10};
+	
+	printarr(arr,sizeof(arr)/4);
+	
+	for(int i = 0; i<sizeof(arr)/4 - 1; i++)
+	{
+		for(int j = i+1; j>0; j--)
+		{
+			if(arr[j-1] >= arr[j])
+			{
+				swap(&arr[j-1],&arr[j]);
+				printarr(arr,sizeof(arr)/4);
+			}
+			else
+			{
+				break;
+			}
+		}
+		printf("\n");
+	}
+
+	printarr(arr,sizeof(arr)/4);
+
+}
+void swap(int * a, int * b)
+{
+	int tmp = 0;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+}
+
+void printarr(int * arr, int size)
+{
+	printf("arr = {");
+	for(int i = 0; i<size; i++)
+	{
+		printf(" %d ",arr[i]);
+	}
+	printf("}\n");
 }
