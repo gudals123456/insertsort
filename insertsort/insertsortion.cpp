0ac543d6d416a07d3fd15af23e65ba17bf2cc96e
hyoungmin <gudals123456@gmail.com>
2015-01-18 00:17:02 +0900

    insert

--- insertsort/insertsortion.cpp
+++ insertsort/insertsortion.cpp
@@ -6,26 +6,29 @@ void printarr(int * arr,int size);
 void main() {
 
 	printf("hello C world!\n");
-	printf("Insert sort\n\n");
+	printf("Insertion sort\n\n");
 
 	int arr[] = {34,27,35,1,23,19,85,67,4,10};
 	
 	printarr(arr,sizeof(arr)/4);
 	
-	for(int i = 0; i<sizeof(arr)/4 - 1; i++)
+	for(int i = 0; i < sizeof(arr)/4 ; i++)
 	{
-		for(int j = i+1; j>0; j--)
-		{
-			if(arr[j-1] >= arr[j])
+		int tmp = arr[i];
+		int j = 0;
+		for(j = i; j>0; j--)
+		{	
+			if(tmp <= arr[j-1])
 			{
-				swap(&arr[j-1],&arr[j]);
-				printarr(arr,sizeof(arr)/4);
+				arr[j] = arr[j-1];
 			}
 			else
 			{
 				break;
 			}
 		}
+		arr[j] = tmp;
+		printarr(arr,sizeof(arr)/4);
 		printf("\n");
 	}
 
